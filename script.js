fae2e4be3d27fce8d4db1878191dc662d799f78f
Danilo-Pereira-of <danilosantosadm95@gmail.com>
2023-06-23 00:35:24 -0300

    Portfolio-Danilo-Pereira

---
+++ script.js
@@ -0,0 +1,22 @@
+
+//taget icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+//scroll sections
+
+window.onscroll = () => {
+    //sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
